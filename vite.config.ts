c9eefeeba63ad6b88fe1f6d956a2086fec16914b
dadda <zivanovicdordje@gmail.com>
2026-02-15 15:47:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
+// Define __dirname for ES modules environment
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Indodjija/', // <- OBAVEZNO dodaj ovo
       server: {
         port: 3000,
         host: '0.0.0.0',
